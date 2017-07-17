4856daf8a9b170b61bfc9d5649bfbf53aa110956
sjnya <sjnyas@gmail.com>
2017-07-18 00:22:44 +0900

    fix bug of duplicated queue item

--- app/src/main/java/com/sjn/stamp/ui/fragment/media_list/QueueListFragment.java
+++ app/src/main/java/com/sjn/stamp/ui/fragment/media_list/QueueListFragment.java
@@ -7,13 +7,10 @@ import com.sjn.stamp.media.QueueManager;
 import com.sjn.stamp.media.provider.ProviderType;
 import com.sjn.stamp.ui.activity.DrawerMenu;
 import com.sjn.stamp.ui.item.QueueTitleItem;
-import com.sjn.stamp.ui.item.SongItem;
 import com.sjn.stamp.utils.LogHelper;
 import com.sjn.stamp.utils.MediaIDHelper;
 
 import java.util.List;
-
-import eu.davidea.flexibleadapter.items.AbstractFlexibleItem;
 
 public class QueueListFragment extends SongListFragment {
 
@@ -29,23 +26,16 @@ public class QueueListFragment extends SongListFragment {
         try {
             LogHelper.d(TAG, "fragment onChildrenLoaded, parentId=" + parentId +
                     "  count=" + children.size());
-            mItemList.clear();
-            mAdapter.clear();
-            String baseMediaId = "";
-            for (MediaBrowserCompat.MediaItem item : children) {
-                AbstractFlexibleItem songItem = new SongItem(item);
-                mItemList.add(songItem);
-                mAdapter.addItem(songItem);
-                if (item.getDescription().getExtras() != null) {
-                    baseMediaId = item.getDescription().getExtras().getString(QueueManager.META_DATA_KEY_BASE_MEDIA_ID);
+            if (children != null && !children.isEmpty()) {
+                String baseMediaId = children.get(0).getDescription().getExtras().getString(QueueManager.META_DATA_KEY_BASE_MEDIA_ID);
+                ProviderType providerType = MediaIDHelper.getProviderType(baseMediaId);
+                if (providerType != null && baseMediaId != null) {
+                    mAdapter.showLayoutInfo(
+                            new QueueTitleItem(providerType, MediaIDHelper.extractBrowseCategoryValueFromMediaID(baseMediaId)), true);
                 }
             }
-            mAdapter.notifyDataSetChanged();
-            ProviderType providerType = MediaIDHelper.getProviderType(baseMediaId);
-            if (providerType != null && baseMediaId != null) {
-                mAdapter.showLayoutInfo(
-                        new QueueTitleItem(providerType, MediaIDHelper.extractBrowseCategoryValueFromMediaID(baseMediaId)), true);
-            }
+            mSongList = children;
+            draw();
         } catch (Throwable t) {
             LogHelper.e(TAG, "Error on childrenloaded", t);
         }
