a23d8dab739460031d6510a13743d29dafd86d69
sjnya <sjnyas@gmail.com>
2017-08-19 17:48:53 +0900

    clarify database value of CategoryType

--- app/src/main/java/com/sjn/stamp/constant/CategoryType.kt
+++ app/src/main/java/com/sjn/stamp/constant/CategoryType.kt
@@ -2,15 +2,12 @@ package com.sjn.stamp.constant
 
 import android.support.v4.media.MediaMetadataCompat
 
-enum class CategoryType constructor(val value: String, val key: String) {
+enum class CategoryType constructor(val databaseValue: String, val key: String) {
     ALBUM("album", MediaMetadataCompat.METADATA_KEY_ALBUM),
     ARTIST("artist", MediaMetadataCompat.METADATA_KEY_ARTIST),
     GENRE("genre", MediaMetadataCompat.METADATA_KEY_GENRE);
 
-
     companion object {
-
-        fun of(value: String?): CategoryType? =
-                CategoryType.values().firstOrNull { it.value == value }
+        fun of(databaseValue: String?): CategoryType? = CategoryType.values().firstOrNull { it.databaseValue == databaseValue }
     }
 }
--- app/src/main/java/com/sjn/stamp/db/dao/CategoryStampDao.kt
+++ app/src/main/java/com/sjn/stamp/db/dao/CategoryStampDao.kt
@@ -7,14 +7,14 @@ import io.realm.Realm
 
 object CategoryStampDao : BaseDao() {
     fun findCategoryStampList(realm: Realm, categoryType: CategoryType, categoryValue: String): List<CategoryStamp> =
-            realm.where(CategoryStamp::class.java).equalTo("type", categoryType.value).equalTo("value", categoryValue).findAll()
+            realm.where(CategoryStamp::class.java).equalTo("type", categoryType.databaseValue).equalTo("value", categoryValue).findAll()
 
     fun findAll(realm: Realm): List<CategoryStamp> =
             realm.where(CategoryStamp::class.java).findAll()
 
     fun remove(realm: Realm, name: String, categoryType: CategoryType, categoryValue: String, isSystem: Boolean) {
         realm.beginTransaction()
-        realm.where(CategoryStamp::class.java).equalTo("name", name).equalTo("type", categoryType.value).equalTo("value", categoryValue).equalTo("isSystem", isSystem).findAll().deleteAllFromRealm()
+        realm.where(CategoryStamp::class.java).equalTo("name", name).equalTo("type", categoryType.databaseValue).equalTo("value", categoryValue).equalTo("isSystem", isSystem).findAll().deleteAllFromRealm()
         realm.commitTransaction()
     }
 
@@ -29,11 +29,11 @@ object CategoryStampDao : BaseDao() {
             return
         }
         realm.beginTransaction()
-        var categoryStamp: CategoryStamp? = realm.where(CategoryStamp::class.java).equalTo("name", name).equalTo("type", categoryType.value).equalTo("value", categoryValue).findFirst()
+        var categoryStamp: CategoryStamp? = realm.where(CategoryStamp::class.java).equalTo("name", name).equalTo("type", categoryType.databaseValue).equalTo("value", categoryValue).findFirst()
         if (categoryStamp == null) {
             categoryStamp = realm.createObject(CategoryStamp::class.java, getAutoIncrementId(realm, CategoryStamp::class.java))
             categoryStamp.name = name
-            categoryStamp.type = categoryType.value
+            categoryStamp.type = categoryType.databaseValue
             categoryStamp.isSystem = isSystem
             categoryStamp.value = categoryValue
         }
